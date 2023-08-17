495df7f1b946f0a8ed3358ee65eacdf62c00a519
volcanoeyes <130888597+volcanoeyes@users.noreply.github.com>
2023-08-17 22:39:17 +0800

    Added write_rap_battle function

--- scripts/calling_gpt.py
+++ scripts/calling_gpt.py
@@ -1,9 +1,20 @@
-class GPT:
+import openai
+class AI_prompt:
     """
     ChatGPT Proompter
     """
     def __init__(self) -> None:
-        pass
+        openai.api_key = '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'
 
-    def prompt(self):
-        return ...
+    def write_rap_battle(self, person1, person2):
+        messages = [{"role": "system", "content": 
+                      "You are a rap battle writer."},
+                     {"role": "user", "content":
+                      f"Write me a rap battle between {person1} and {person2}"}
+              ]
+        chat = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=messages
+        )
+        reply = chat.choices[0].message.content
+        return reply
+        
