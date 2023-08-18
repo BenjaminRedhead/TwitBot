0cfa0f9da7ca85f06222e39042c5b5a8121108db
volcanoeyes <130888597+volcanoeyes@users.noreply.github.com>
2023-08-18 21:12:33 +0800

    Added scrape_bbc_article function

--- scripts/retrieving_data.py
+++ scripts/retrieving_data.py
@@ -30,9 +30,26 @@ def scrape_bbc_headlines():
 
 def scrape_bbc_article(url):
     page = requests.get(url)
-    
+    soup = BeautifulSoup(page.content, "html.parser")
+    paragraphs = soup.find_all("p", class_="ssrcss-1q0x1qg-Paragraph e1jhz7w10")
+    article_text = ""
+    for p in paragraphs:
+        article_text += p.text + "\n"
+    return article_text
 
+print(scrape_bbc_article("https://www.bbc.com/news/world-asia-66517901"))
+"""
 headlines = scrape_bbc_headlines()
 for i in headlines:
     print(f"{i}: {headlines[i]}")
+"""
+
+
+
+
+
+
+
+
+
 
